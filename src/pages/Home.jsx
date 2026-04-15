dbf05f089b3cc2ceb56e04c5a495571f74ffd3d6
Sandra <sandra.mstowskaa@gmail.com>
2026-04-15 14:34:27 +0200

    feat(Home): add photo section tools

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -161,6 +161,21 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <section
+        className="relative h-[40vh] sm:h-[45vh] md:h-[50vh] lg:h-[60vh] w-full bg-cover bg-bottom bg-no-repeat"
+        style={{
+          backgroundImage: "url('/assets/background-section-tools.svg')",
+        }}
+      >
+        <div className="absolute inset-0 bg-white/40"></div>
+
+        <div className="relative z-10 flex h-full items-center justify-start pl-8">
+          <h2 className="font-mont px-4 md:px-8 text-xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold text-[#fafff4] tracking-wider drop-shadow-lg">
+            Najlepszy sprzęt.
+          </h2>
+        </div>
+      </section>
     </>
   );
 };
